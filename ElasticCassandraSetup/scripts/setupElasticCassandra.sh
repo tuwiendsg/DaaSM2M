1591eef7a188d54a46a47cab2b3a97c25a95f581
Daniel Moldovan <d.moldovan@dsg.tuwien.ac.at>
2014-01-30 15:11:00 +0100

    Moving cassandra instantiation script from rc.local to inid.t

--- ElasticCassandraSetup/scripts/setupElasticCassandra.sh
+++ ElasticCassandraSetup/scripts/setupElasticCassandra.sh
@@ -35,31 +35,18 @@ select REPLY in "Seed (Main Node)" "Data Node (Slave)" ; do
 case $REPLY in
         'Seed (Main Node)' )
  
-           #Ensures that if a VM image is created and booted, at boot time the machine IP will be inserted in Cassandra config file
-	   sudo -S sed -i 's/exit 0//' /etc/rc.local 
-	   echo "eval \"sed -i.bak 's/listen_address:.*/listen_address: \`ifconfig eth0 | grep -o 'inet addr:[0-9.]*' | grep -o [0-9.]*\`/' $CASSANDRA_CONFIG/cassandra.yaml\"" | sudo -S tee -a /etc/rc.local
-           echo "eval \"sed -i.bak 's/- seeds:.*/- seeds: \\\"\`ifconfig eth0 | grep -o 'inet addr:[0-9.]*' | grep -o [0-9.]*\`\\\"/' $CASSANDRA_CONFIG/cassandra.yaml\"" | sudo -S tee -a /etc/rc.local
-	   echo "eval \"sed -i.bak 's/rpc_address:.*/rpc_address: \`ifconfig eth0 | grep -o 'inet addr:[0-9.]*' | grep -o [0-9.]*\`/' $CASSANDRA_CONFIG/cassandra.yaml\"" | sudo -S tee -a /etc/rc.local
-           echo "eval \"sed -i 's/\<native_transport_port:.*/native_transport_port: $CASSANDRA_NATIVE_CQL_PORT/' $CASSANDRA_CONFIG/cassandra.yaml\"" | sudo -S tee -a /etc/rc.local
-           echo "eval \"sed -i 's/\<rpc_port:.*/rpc_port: $CASSANDRA_RPC_PORT/' $CASSANDRA_CONFIG/cassandra.yaml\"" | sudo -S tee -a /etc/rc.local
-	   echo "eval \"sed -i 's/\<storage_port:.*/storage_port: $CASSANDRA_TCP_PORT/' $CASSANDRA_CONFIG/cassandra.yaml\"" | sudo -S tee -a /etc/rc.local               
-	   echo "su $USER -c $CURRENT_DIR/createCassandraKeyspace.sh" | sudo -S tee -a /etc/rc.local
-           echo "exit 0" | sudo -S tee -a /etc/rc.local
-
-           sudo -S chmod +x ./createDefaultCassandraKeyspace.sh 
-	
 	   #apply configuration
 	   #/etc/rc.local
  
            #prepare script that scales down the cluster by removing the less loaded node
            eval "sed -i 's#\<CASSANDRA_BIN=.*#CASSANDRA_BIN=$CASSANDRA_BIN#' ./scaleInCluster.sh"
-           eval "sed -i 's#\<CASSANDRA_BIN=.*#CASSANDRA_BIN=$CASSANDRA_BIN#' ./createDefaultCassandraKeyspace.sh"
-           eval "sed -i 's#\<CURRENT_DIR=.*#CURRENT_DIR=$CURRENT_DIR#' ./createDefaultCassandraKeyspace.sh"
-   		
+	   eval "sed -i 's#\<CASSANDRA_HOME=.*#CASSANDRA_HOME=$CASSANDRA_HOME#' ./cassandra" 
+            
+           #eval "sed -i 's#\<CASSANDRA_BIN=.*#CASSANDRA_BIN=$CASSANDRA_BIN#' ./createDefaultCassandraKeyspace.sh"
+           #eval "sed -i 's#\<CURRENT_DIR=.*#CURRENT_DIR=$CURRENT_DIR#' ./createDefaultCassandraKeyspace.sh"
 
            sudo -S chmod +x ./scaleInCluster.sh
            sudo -S cp ./scaleInCluster.sh /bin/scaleIn 
-      
  
            #prepare script for decomissioning node
 	   # execute 'decomission CASSANDRA_NODE_IP' before removing Cassandra Node, to ensure proper cleanup	
@@ -72,41 +59,34 @@ case $REPLY in
 	   sudo -S cp ./cassandra /etc/init.d/cassandra
 	   sudo -S chmod +x /etc/init.d/cassandra
 	   sudo -S update-rc.d cassandra defaults
-           #does NOT start cassandra automatically, as not to generate tokens and linked to one token to a particular IP, which is bad when cloning/snapshoting the VM
-      
-           echo "If this is not to be snapshotted and used in connection with YCSB please reboot OS (sudo reboot) to finish configuration".
-
+            
            break;;
 
 
         'Data Node (Slave)' )
            
            #prepare script for gathering seed and current IP and configuring Cassandra
-           eval "sed -i 's#\<CASSANDRA_CONFIG=.*#CASSANDRA_CONFIG=$CASSANDRA_CONFIG#' ./joinRing.sh";
-           eval "sed -i 's#\<CASSANDRA_BIN=.*#CASSANDRA_BIN=$CASSANDRA_BIN#' ./joinRing.sh";
+           eval "sed -i 's#\<CASSANDRA_CONFIG=.*#CASSANDRA_CONFIG=$CASSANDRA_CONFIG#' ./joinRing";
+           eval "sed -i 's#\<CASSANDRA_BIN=.*#CASSANDRA_BIN=$CASSANDRA_BIN#' ./joinRing";
 	   
 	   #execute the specified command for retrieving user supplied VM contextualization data
-           eval "sed -i 's#\<cassandraSeedIPSource=.*#cassandraSeedIPSource=\"$CASSANDRA_SEED_IP_SOURCE\"#' ./joinRing.sh";            
+           eval "sed -i 's#\<cassandraSeedIPSource=.*#cassandraSeedIPSource=\"$CASSANDRA_SEED_IP_SOURCE\"#' ./joinRing";            
 
            #Ensures that if a VM image is created and booted, at boot time the machine IP will be inserted in Cassandra config file by inserting in the /etc/rc.local
-           sudo -S sed -i 's/exit 0//' /etc/rc.local 
-           echo "eval \"sed -i.bak 's/listen_address:.*/listen_address: \`ifconfig eth0 | grep -o 'inet addr:[0-9.]*' | grep -o [0-9.]*\`/' $CASSANDRA_CONFIG/cassandra.yaml\"" | sudo -S tee -a /etc/rc.local
-           echo "eval \"sed -i.bak 's/rpc_address:.*/rpc_address: \`ifconfig eth0 | grep -o 'inet addr:[0-9.]*' | grep -o [0-9.]*\`/' $CASSANDRA_CONFIG/cassandra.yaml\"" | sudo -S tee -a /etc/rc.local
-           echo "eval \"sed -i 's/rpc_port:.*/rpc_port: $CASSANDRA_RPC_PORT/' $CASSANDRA_CONFIG/cassandra.yaml\"" | sudo -S tee -a /etc/rc.local
-	   echo "eval \"sed -i 's/storage_port:.*/storage_port: $CASSANDRA_TCP_PORT/' $CASSANDRA_CONFIG/cassandra.yaml\"" | sudo -S tee -a /etc/rc.local         
-           echo "exec $CURRENT_DIR/joinRing.sh" | sudo -S tee -a /etc/rc.local
-           echo "exit 0" | sudo -S tee -a /etc/rc.local
+           
 	   #execute rc.local to configure local iP
            #/etc/rc.local
-           sudo -S chmod +x ./joinRing.sh
-           sudo -S ln -s $CURRENT_DIR/joinRing.sh /bin/joinRing
+           sudo -S cp ./cassandra /etc/init.d/joinRing
+	   sudo -S chmod +x /etc/init.d/joinRing
+	   sudo -S update-rc.d joinRing defaults
+           
            #if node, ensure it does not start automatically
            if [ -f /etc/init.d/cassandra ]
 		then
 		   sudo rm /etc/init.d/cassandra
            fi     
            #NOTE: the cassandra service is not started yet, as not to generate any load token yet. To start the service one must call "joinRing".
-           echo "If this is not to be snapshotted, please reboot OS (sudo reboot) to finish configuration".
+           echo "If this is not to be snapshotted, please reboot OS (sudo reboot) to finish configuration. Else, run sudo service joinRing start".
            break;;
 esac
 done
