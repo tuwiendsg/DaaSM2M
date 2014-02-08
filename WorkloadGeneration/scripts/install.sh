3c2851ffcacc83296783cd62df3262018cd00c4f
Daniel Moldovan <d.moldovan@dsg.tuwien.ac.at>
2014-02-08 19:23:38 +0100

    Updated last changed scripts.

--- WorkloadGeneration/scripts/install.sh
+++ WorkloadGeneration/scripts/install.sh
@@ -3,7 +3,7 @@
 CURRENT_DIR=$(pwd)
  
 #put daa-service in services
-sudo -S cp ./daas-service /etc/init.d/workload-service
+sudo -S cp ./workload-service /etc/init.d/workload-service
 sudo -S chmod +x /etc/init.d/workload-service
 sudo -S update-rc.d workload-service defaults
 
