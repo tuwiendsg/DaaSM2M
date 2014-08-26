b395955fd6201cc47af4aa978e854a3dee5f84e1
Georgiana Copil <e.copil@dsg.tuwien.ac.at>
2014-08-26 17:58:01 +0200

    fix chef solo

--- IoT_M2MDaaS/M2MDaaS_IoT Analysis/run_mqtt_broker.sh
+++ IoT_M2MDaaS/M2MDaaS_IoT Analysis/run_mqtt_broker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd "/home/ubuntu"
-#sudo wget "http://128.130.172.215/salsa/upload/files/DaasService/IoT/solo-install.sh"
-#sudo chmod +x ./solo-install.sh
-#sudo ./solo-install.sh >> /tmp/salsa.artifact.log
+sudo wget "http://128.130.172.215/salsa/upload/files/DaasService/IoT/solo-install.sh"
+sudo chmod +x ./solo-install.sh
+sudo ./solo-install.sh >> /tmp/salsa.artifact.log
 sudo wget "http://128.130.172.215/salsa/upload/files/DaasService/IoT/chef-mqtt_broker.tar.gz" >> /tmp/salsa.artifact.log
 sudo tar -xzf ./chef-mqtt_broker.tar.gz >> /tmp/salsa.artifact.log
 sudo echo "Untarred mqttbroker " >> /tmp/salsa.artifact.log
--- IoT_M2MDaaS/M2MDaaS_IoT Analysis/run_sensor.sh
+++ IoT_M2MDaaS/M2MDaaS_IoT Analysis/run_sensor.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd "/home/ubuntu"
-#sudo wget http://128.130.172.215/salsa/upload/files/DaasService/IoT/solo-install.sh
-#sudo chmod +x ./solo-install.sh
-#sudo ./solo-install.sh >> /tmp/salsa.artifact.log
+sudo wget http://128.130.172.215/salsa/upload/files/DaasService/IoT/solo-install.sh
+sudo chmod +x ./solo-install.sh
+sudo ./solo-install.sh >> /tmp/salsa.artifact.log
 sudo wget http://128.130.172.215/salsa/upload/files/DaasService/IoT/chef-sensor.tar.gz
 sudo tar -xzf ./chef-sensor.tar.gz
 sudo echo "finished untaring sensor " >> /tmp/salsa.artifact.log
