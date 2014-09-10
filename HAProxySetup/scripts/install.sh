0e82b82e62bc307a937e048d731297bba268ca48
Daniel Moldovan <d.moldovan@dsg.tuwien.ac.at>
2014-09-10 18:06:32 +0200

    Updated install to install python not scripts for reconfiguring HAProxy

--- HAProxySetup/scripts/install.sh
+++ HAProxySetup/scripts/install.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 CURRENT_DIR=$(pwd)
-
-#please uncomment if needed as appropriate for your distro. otherwise HAProxy 1.4 will be installed which does not work with our ganglia plug-in
-
-#for Ubuntu devel, lucid, precise, saucy
-#sudo -S add-apt-repository ppa:vbernat/haproxy-1.5
+ 
+sudo -S add-apt-repository ppa:vbernat/haproxy-1.5 -y
 
 #for Ubuntu  raring, quantal, precise, oneiric 
 #sudo -S add-apt-repository ppa:nilya/haproxy-1.5
 
-#sudo -S apt-get update
-
+sudo -S apt-get update
+sudo -S apt-get install ganglia-monitor gmond 
 sudo -S apt-get install haproxy
+sudo -S apt-get install python
+sudo -S apt-get install python-virtualenv
+sudo -S pip install Flask
 
 #set HAProxy config path in 
-eval "sed -i 's#HAPROXY_CONFIG_FILE=.*#HAPROXY_CONFIG_FILE=$CURRENT_DIR/haproxyConfig#' $CURRENT_DIR/registerToHAProxy.sh"
-eval "sed -i 's#HAPROXY_CONFIG_FILE=.*#HAPROXY_CONFIG_FILE=$CURRENT_DIR/haproxyConfig#' $CURRENT_DIR/deregisterToHAProxy.sh"
-
-#copy scripts for registering and deregistering to HAPROXY in /bin
-sudo -S chmod +x $CURRENT_DIR/registerToHAProxy.sh
-sudo -S chmod +x $CURRENT_DIR/deregisterToHAProxy.sh
-sudo -S cp $CURRENT_DIR/registerToHAProxy.sh /bin/registerToHAProxy
-sudo -S cp $CURRENT_DIR/deregisterToHAProxy.sh /bin/deregisterToHAProxy
-
+eval "sed -i 's#HAPROXY_CONFIG_FILE=.*#HAPROXY_CONFIG_FILE=$CURRENT_DIR/haproxyConfig#' $CURRENT_DIR/configPythonRESTfulAPI.py"
+eval "sed -i 's#HAPROXY_CONFIG_FILE=.*#HAPROXY_CONFIG_FILE=$CURRENT_DIR/haproxyConfig#' $CURRENT_DIR/configPythonRESTfulAPI.py"
+ 
 haproxy -f $CURRENT_DIR/haproxyConfig
+
+python ./configPythonRESTfulAPI.py  &
+
+cd ./gangliaPlugIns
+./setupPlugIns.sh
+sudo -S service ganglia-monitor restart
 
